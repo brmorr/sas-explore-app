328de779a3bdba30118579a155a6caa0cbc82600
Brad Morris <Bradley.Morris@sas.com>
2023-08-20 13:00:48 -0400

    demo checkpoint 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import { Dashboard } from "./components/Dashboard";
-import config from "./config.json";
 
 function App() {
-  return <Dashboard packageUri={`${process.env.PUBLIC_URL}/reportPackages/${config.REPORT_UID}`}></Dashboard>;
+  return (
+    <sas-report
+      packageUri={`${process.env.PUBLIC_URL}/reportPackages/cf1c3bc8-ccad-4063-aa81-ec3eb53a970a`}
+    ></sas-report>
+  );
 }
 
 export default App;
